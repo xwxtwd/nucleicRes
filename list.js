9f1f1e9e34d6fd33ef21614237c4ed99f194ec6c
J.Son <x@x-wx.com>
2022-10-26 23:24:31 +0800

    rrr

--- list.js
+++ list.js
@@ -16,34 +16,34 @@ let body = $response.body;
 if (body) {
     console.log(3333)
 
-        nowDate = new Date();
-        function padding(num, length) {
-            //这里用slice和substr均可
-            return (Array(length).join("0") + num).slice(-length);
-        }
-        var date = {
-            year: nowDate.getFullYear(),
-            month: padding(nowDate.getMonth() + 1, 2),
-            date: padding(nowDate.getDate(), 2),
-        }
-    
-    
-    
-    
-        var body = JSON.parse($response.body || '{}') || {}
-        var data = body.data || []
-        var firstItem = data[0]
-        if (firstItem) {
-    
-    
-            firstItem.testing_date.replace(/\d{4}-\d{1,2}-\d{1,2}/, [date.year - 1, date.month, date.date].join('-'))
-            $done({
-                body: JSON.stringify(body)
-            });
-        } else {
-            // $done({});
-        }
-  
+    // nowDate = new Date();
+    // function padding(num, length) {
+    //     //这里用slice和substr均可
+    //     return (Array(length).join("0") + num).slice(-length);
+    // }
+    // var date = {
+    //     year: nowDate.getFullYear(),
+    //     month: padding(nowDate.getMonth() + 1, 2),
+    //     date: padding(nowDate.getDate(), 2),
+    // }
+
+
+
+
+    // var body = JSON.parse($response.body || '{}') || {}
+    // var data = body.data || []
+    // var firstItem = data[0]
+    // if (firstItem) {
+
+
+    //     firstItem.testing_date.replace(/\d{4}-\d{1,2}-\d{1,2}/, [date.year - 1, date.month, date.date].join('-'))
+    //     $done({
+    //         body: JSON.stringify(body)
+    //     });
+    // } else {
+    //     // $done({});
+    // }
+
 
 } else {
     console.log(666)
